1d98f432869ded3b9154baf4bedd89b1bbb3944a
Andrew-Jin <D29Jin@uwaterloo.ca>
2017-02-04 16:37:25 -0500

    Created profile page xml

--- app/src/main/java/com/saff/sociallyawkwardfriendfinder/MainActivity.java
+++ app/src/main/java/com/saff/sociallyawkwardfriendfinder/MainActivity.java
@@ -3,27 +3,38 @@ package com.saff.sociallyawkwardfriendfinder;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CaptureRequest;
 import android.media.Image;
+import android.media.ImageReader;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageButton cameraButton = (ImageButton) findViewById(R.id.camera_button);
+    ImageButton profileButton = (ImageButton) findViewById(R.id.profile_button);
+    Button findButton = (Button) findViewById(R.id.find_button);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageButton cameraButton = (ImageButton) findViewById(R.id.camera_button);
-        ImageButton profileButton = (ImageButton) findViewById(R.id.profile_button);
-        Button findButton = (Button) findViewById(R.id.find_button);
+
 
         if (cameraButton.callOnClick()) {
             UseCamera();
@@ -41,39 +52,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void UseCamera(){
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        }
+        cameraPermission();
 
-//        ImageButton imageButton = (ImageButton) findViewById(R.id.camera_button);
-//      if (ContextCompat.checkSelfPermission(this,
-//        Manifest.permission.CAMERA)
-//        != PackageManager.PERMISSION_GRANTED) {
-//
-//            // Should we show an explanation?
-//            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-//                    Manifest.permission.CAMERA)) {
-//
-//                // Show an explanation to the user *asynchronously* -- don't block
-//                // this thread waiting for the user's response! After the user
-//                // sees the explanation, try again to request the permission.
-//
-//            } else {
-//
-//                // No explanation needed, we can request the permission.
-//
-//                ActivityCompat.requestPermissions(this,
-//                        new String[]{Manifest.permission.CAMERA}, );
-//
-//                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-//                // app-defined int constant. The callback method gets the
-//                // result of the request.
-//            }
-//        }
+    }
+
+    void cameraPermission(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA}, 1);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
     }
 
     void search(){
 
     }
+
+    private CameraDevice cameraDevice;
+    private CaptureRequest.Builder previewBuilder;
+    private CameraCaptureSession previewSession;
+
 }
--- app/src/main/java/com/saff/sociallyawkwardfriendfinder/ProfilePage.java
+++ app/src/main/java/com/saff/sociallyawkwardfriendfinder/ProfilePage.java
@@ -1,11 +1,29 @@
 package com.saff.sociallyawkwardfriendfinder;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 /**
  * Created by andrew on 2/4/17.
  */
 
 public class ProfilePage extends Activity {
+
+    EditText first_name;
+    EditText last_name;
+    RadioButton Gender;
+    Button submit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_page);
+
+
+    }
+
 
 }
